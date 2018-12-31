5a8cef3e930bca6d906a173b832ca73d4a407e2b
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2018-12-31 18:37:12 -0200

    Adiciona a rota para remover um autor.

--- server/router/autor.js
+++ server/router/autor.js
@@ -4,18 +4,33 @@ const Autor = require('../model/models/Autor');
 
 router.get('/', (req, res) => {
   Autor.find()
-  .then(autor => res.status(200).json({autor}));
+    .then(autor => res.status(200).json({
+      autor
+    }));
 });
 
- router.get('/:id', (req, res) => {
-   const idAutor = req.params.id
-   Autor.findById(idAutor)
-     .then((autor) => {
-       res.status(200).json({
-         autor
-       })
-     }).catch((erro) => {res.status(406).json({message: '406 Not Acceptable', erro})});
- });
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  Autor.remove({
+      _id: id
+    })
+    .then(resultado => res.status(200).json(resultado));
+});
+
+router.get('/:id', (req, res) => {
+  const idAutor = req.params.id
+  Autor.findById(idAutor)
+    .then((autor) => {
+      res.status(200).json({
+        autor
+      })
+    }).catch((erro) => {
+      res.status(406).json({
+        message: '406 Not Acceptable',
+        erro
+      })
+    });
+});
 
 router.post('/', (req, res) => {
   const autor = new Autor({
@@ -24,8 +39,13 @@ router.post('/', (req, res) => {
     password: req.body.password,
   });
   autor.save()
-    .then(result => res.status(201).json({result}))
-    .catch((erro) => res.status(406).json({message: '406 Not Acceptable', erro}));
+    .then(result => res.status(201).json({
+      result
+    }))
+    .catch((erro) => res.status(406).json({
+      message: '406 Not Acceptable',
+      erro
+    }));
 });
 
 module.exports = router;
