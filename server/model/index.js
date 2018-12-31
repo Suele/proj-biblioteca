eb3a95999d2647c45d1b83eb9b9f19109c939245
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2018-12-31 12:30:34 -0200

    Cria a model do Autor.

---
+++ server/model/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/biblioteca' , { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, '>>> Error connection in MongoDB:'));
+db.once('open', () => {
+  console.log('>>> Connect MongoDB.')
+});
+
+module.exports = mongoose;
+
+
---
+++ server/model/models/Autor.js
@@ -0,0 +1,14 @@
+const mongoose = require('../index');
+var Schema = mongoose.Schema;
+
+const AutorSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+  date: {
+    type: Date,
+    dafault: Date.now
+  }
+});
+
+module.exports = mongoose.model('Autor', AutorSchema);
